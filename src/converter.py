bf100569a4c932e92a0da098eac5a042162c0792
Husni Ahamed <141121519+Husniahmed10@users.noreply.github.com>
2025-04-22 20:07:39 +0530

    Update converter.py

--- src/converter.py
+++ src/converter.py
@@ -1,6 +1,6 @@
 import requests
 
-def get_exchange_rate(base_currency, ):
+def get_exchange_rate(base_currency, target_currency):
     url = f"https://api.exchangerate.host/latest?base={base_currency}&symbols={target_currency}"
     response = requests.get(url)
     data = response.json()
